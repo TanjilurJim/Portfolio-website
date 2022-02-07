ba513a36a857c1018bf850c574e524e9746dd4e6
TanjilurJim <tanjilurrahman21@gmail.com>
2022-02-08 04:19:00 +0600

    fixed some mistakes

--- js/index.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    fetch(document.location.href)
-        .then(res => {
-            const last_edit = res.headers.get("last-modified")
-            document.getElementById("last-edit").innerHTML = "Last Modified: "+ last_edit
-        })
-}
